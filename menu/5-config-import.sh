8bcea5b6f5cb1925e8fe4eece7911aa5e6e86e31
David7ce <79609649+David7ce@users.noreply.github.com>
2024-03-27 02:45:56 +0000

    add dirs and remove extra line on config-import.sh

--- menu/5-config-import.sh
+++ menu/5-config-import.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# create directories
+sudo mkdir -p ~/.config/{chromium,GIMP,hypr,kitty,mpv,neofetch,nvim,obs-studio,rofi}
+sudo mkdir -p ~/.mozilla
+
 # copy config files
 sudo cp -r config-files/.config ~/
 sudo cp -r config-files/.var ~/
-
-# remove import
-rm ~/import.sh
 
 # Change ownership
 sudo chown -R $USER: ~/.config/ ~/.var/
